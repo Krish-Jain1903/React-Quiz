5bfa4e1d0a8b6789a8f1e3674b15e0f253bb0b6f
KrishJain <krishjain1903@gmail.com>
2025-04-15 23:57:31 +0530

    Creating your Own Custom Fake Api for React Quiz

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
 // import DateCounter from "./DateCounter";
+import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
 
+const initialState = {
+  questions: [],
+  status: "loading",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "dataReceived":
+      break;
+    default:
+      break;
+  }
+}
+
 export default function App() {
   // return <div>{/* <DateCounter /> */}</div>;
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  useEffect(function () {
+    fetch("http://localhost:8000/questions")
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: "dataRecived", payload: data }))
+      .catch((err) => console.log("Error"));
+  }, []);
+
   return (
     <div className="app">
       <Header />
