63dee34b52055445ac8ad66b77e60758be2e5c0c
KrishJain <krishjain1903@gmail.com>
2025-04-16 23:59:07 +0530

    Strated Showing Quiz Questions

--- src/App.js
+++ src/App.js
@@ -2,18 +2,27 @@
 import { useEffect, useReducer } from "react";
 import Header from "./Header";
 import Main from "./Main";
+import Loader from "./Loader";
+import Error from "./Error";
+import StartScreen from "./data/StartScreen";
+import Question from "./Question";
 
 const initialState = {
   questions: [],
   status: "loading",
+  index: 0,
 };
 
 function reducer(state, action) {
   switch (action.type) {
-    case "dataReceived":
-      break;
+    case "dataRecived":
+      return { ...state, questions: action.payload, status: "ready" };
+    case "dataFailed":
+      return { ...state, status: "error" };
+    case "start":
+      return { ...state, status: "active" };
     default:
-      break;
+      throw new Error("Unknown Action");
   }
 }
 
@@ -21,18 +30,27 @@ export default function App() {
   // return <div>{/* <DateCounter /> */}</div>;
 
   const [state, dispatch] = useReducer(reducer, initialState);
+  const { questions, status, index } = state;
+  const numQuestions = questions.length;
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
       .then((res) => res.json())
       .then((data) => dispatch({ type: "dataRecived", payload: data }))
-      .catch((err) => console.log("Error"));
+      .catch((err) => dispatch({ type: "dataFailed" }));
   }, []);
 
   return (
     <div className="app">
       <Header />
-      <Main />
+      <Main>
+        {status === "loading" && <Loader />}
+        {status === "error" && <Error />}
+        {status === "ready" && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === "active" && <Question question={questions[index]} />}
+      </Main>
     </div>
   );
 }
---
+++ src/Question.js
@@ -0,0 +1,6 @@
+function Question({ question }) {
+  console.log(question);
+  return <div>Question</div>;
+}
+
+export default Question;
